e71685168c2c91053c6d1e36fb9252ae1b04844e
AbroadUmedaShota <s-umeda@abroad-o.com>
2026-08-13 05:46:53 +0900

    test: プロトコル相対IEシムを検証

--- scripts/lib/ie-shim-contract.mjs
+++ scripts/lib/ie-shim-contract.mjs
@@ -1,6 +1,6 @@
 const forbiddenPatterns = [
-  { label: 'oss.maxcdn.com IE shim', pattern: /https?:\/\/oss\.maxcdn\.com\//i },
-  { label: 'Google Code css3-mediaqueries shim', pattern: /https?:\/\/[^\s"']*googlecode\.com\//i },
+  { label: 'oss.maxcdn.com IE shim', pattern: /(?:https?:)?\/\/oss\.maxcdn\.com\//i },
+  { label: 'Google Code css3-mediaqueries shim', pattern: /(?:https?:)?\/\/[^\s"']*googlecode\.com\//i },
   { label: 'IE conditional comment', pattern: /<!--\s*\[if\s+[^\]]*\bIE\b[^\]]*\]>/i },
   { label: 'IE conditional comment terminator', pattern: /<!\[endif\]\s*-->/i }
 ];
--- scripts/test-ie-shim-contract-negative.mjs
+++ scripts/test-ie-shim-contract-negative.mjs
@@ -8,7 +8,9 @@ test('accepts ordinary external dependencies', () => {
 
 for (const [name, html] of Object.entries({
   maxcdn: '<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>',
+  protocolRelativeMaxcdn: '<script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>',
   googlecode: '<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>',
+  protocolRelativeGooglecode: '<script src="//css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>',
   conditional: '<!--[if lt IE 9]><script src="/js/legacy.js"></script><![endif]-->',
   conditionalTerminator: '<![endif]-->'
 })) {
